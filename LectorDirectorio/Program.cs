6c9e3284caae8f06c4a67d3a91e4324a0f5da5c8
SanchezMariaVictoria2005 <mvictoria14092005@gmail.com>
2026-06-30 04:08:01 -0300

    interfaz para mostrar el nombre de los directorios y el nombre y tamaño en KB de los archivos definido

--- LectorDirectorio/Program.cs
+++ LectorDirectorio/Program.cs
@@ -13,6 +13,36 @@ do
         validarDireccion = true;
         var directorios = Directory.GetDirectories(direccion);//obtengo todos los subdirectorios
         var archivos = Directory.GetFiles(direccion); //obtengo todos los archivos de ese directorio
+
+        if (directorios.Length > 0)
+        {
+            Console.WriteLine("LISTADO DE DIRECTORIOS");
+             foreach (var directorio in directorios) //listo los subdirectorios
+            {
+                Console.WriteLine(Path.GetFileName(directorio));
+            }
+        }
+        else
+        {
+            Console.WriteLine("no hay directorios");
+        }
+
+        if (archivos.Length > 0)
+        {
+            Console.WriteLine("LISTADO DE ARCHIVOS");
+            foreach (var archivo in archivos)//listo los archivos
+            {
+                var info = new FileInfo(archivo) ;
+                var tama = info.Length/1024;
+
+                Console.WriteLine($"nombre:{Path.GetFileName(archivo)} tamaño: {tama} KB");
+            }
+        }
+        else
+        {
+            Console.WriteLine("no hay archivos");
+        } 
+       
     }
     else
     {
