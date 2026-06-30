aaea3077367f23ae33bcb8dc26dcb85c5f628ff3
SanchezMariaVictoria2005 <mvictoria14092005@gmail.com>
2026-06-30 03:25:34 -0300

    interfaz para ingresar la ruta de un directorio y verificar si existe definida

--- LectorDirectorio/Program.cs
+++ LectorDirectorio/Program.cs
@@ -1,1 +1,15 @@
 ﻿Console.WriteLine("Hello, World!");
+
+string direccion;
+bool validarDireccion = false;
+
+do
+{
+    Console.WriteLine("ingrese la ruta del directorio");
+    direccion = Console.ReadLine();
+
+    if (Directory.Exists(direccion))
+    {
+        validarDireccion = true;
+    }
+}while(validarDireccion == false);
